da0b556125fc53d94c6688802b8a9bd15035a0b1
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-05-10 15:27:51 +0300

    fix SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,19 +1,27 @@
+import toast, { Toaster } from "react-hot-toast";
 import s from "./SearchBar.module.css";
 
 const SearchBar = ({ onSubmit }) => {
   const handleSubmit = (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
     const form = e.target;
-    const {search} = form.elements
+    const { search } = form.elements;
 
-    onSubmit({search: search.value})
+    if (search.value === "") {
+       toast.error("Поле для пошуку має бути заповненим");
+       return
+    }
+    onSubmit({ search: search.value });
 
-    form.reset()
+    form.reset();
   };
 
   return (
-    <header className={s.header}>
-      <form className={s.form} onSubmit={handleSubmit}>
+   <> <header className={s.header}>
+      <form
+        className={s.form}
+        onSubmit={handleSubmit}
+      >
         <input
           className={s.input}
           name="search"
@@ -22,11 +30,18 @@ const SearchBar = ({ onSubmit }) => {
           autoFocus
           placeholder="Search images and photos"
         />
-        <button className={s.search_btn} type="submit">
+        <button
+          className={s.search_btn}
+          type="submit"
+        >
           Search
         </button>
       </form>
     </header>
+    <div><Toaster
+      position="top-center"
+      reverseOrder={false}/></div>
+    </>
   );
 };
 
